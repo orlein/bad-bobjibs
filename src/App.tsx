13db6814d3c6f68f8499257170318243a99e6965
orlein <pcj9024@gmail.com>
2024-12-19 21:30:50 +0900

    update tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import { Provider } from 'react-redux';
 import store from './app/store';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Home from './pages/home';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </QueryClientProvider>
   );
 }
 
