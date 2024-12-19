faacb315000b034885a42a9f2414930c20f2f4d0
00iun <lsh681236@naver.com>
2024-12-19 22:05:58 +0900

    login_iun

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { Provider } from 'react-redux';
 import store from './app/store';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Home from './pages/home';
+import Login from './pages/login';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { LoginForm } from './components/login-form';
 
 const queryClient = new QueryClient();
 
@@ -13,6 +15,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
           </Routes>
         </BrowserRouter>
       </Provider>
