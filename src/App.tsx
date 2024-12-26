49e23e20b94f233fd04d8057a2e0a3d4582b81ed
Nagi-ei <nakt4453@gmail.com>
2024-12-26 21:25:25 +0900

    🐛 대소문자, 브라우저 라우터 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router';
+import { Route, Routes } from 'react-router';
 import Home from './pages/home';
-import Map from './pages/Map';
-import Search from './pages/Search';
-import Profile from './pages/Profile';
+import Map from './pages/map';
+import Search from './pages/search';
+import Profile from './pages/profile';
 
 function App() {
   return (
-    // <Router>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/map" element={<Map />} />
       <Route path="/search" element={<Search />} />
       <Route path="/profile" element={<Profile />} />
     </Routes>
-    /* </Router> */
   );
 }
 
