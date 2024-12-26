fd7bcaecba5e53fee16ba56b7147b8886e94d5d8
Nagi-ei <nakt4453@gmail.com>
2024-12-26 20:03:08 +0900

    ✨ Connect main and login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Provider } from 'react-redux';
 import store from './app/store';
-import { BrowserRouter, Route, Routes } from 'react-router';
-import Home from './pages/home';
+import { Route, Routes } from 'react-router';
+// import Home from './pages/home';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Login from './pages/login';
+import Page from './app/dashboard/page';
 
 const queryClient = new QueryClient();
 
@@ -11,12 +12,10 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route index element={<Page />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
       </Provider>
     </QueryClientProvider>
   );
